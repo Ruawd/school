2f0a703fb094c49ca06784d79af63b3116bf1fc4
Ruawd <Ruawd@noreply.gitcode.com>
2026-05-01 19:44:38 +0800

    Fix server timezone for reservations

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config({ quiet: true });
 
+process.env.TZ = process.env.APP_TIMEZONE || 'Asia/Shanghai';
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
